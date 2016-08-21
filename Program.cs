8440c9c7b9e9ecd8be8c60072f34ac0db1ade5d8
Nino Floris <mail@ninofloris.com>
2016-08-21 16:33:01 +0200

    Reproducible results 🎉🎉

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -11,55 +12,74 @@ namespace ConsoleApplication
 {
     public class Program
     {
-        private NpgsqlConnection _conn;
         private string _connString;
+        private NpgsqlConnection _conn;
 
         public static void Main(string[] args)
         {
             var prog = new Program();
+            prog.Init().GetAwaiter().GetResult();
         }
 
-        public void Init()
+        public Task Init()
         {
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("parameters.json");
             _connString = builder.Build().GetConnectionString("postgres");
 
+            _conn = new NpgsqlConnection(_connString);
+            Npgsql.Logging.NpgsqlLogManager.Provider = new Npgsql.Logging.ConsoleLoggingProvider(Npgsql.Logging.NpgsqlLogLevel.Trace, true, true);
+
+            SqlMapper.AddTypeHandler(typeof(Json), new JsonTypeHandler());
+
             InitFixtures();
-            RunQuery();
+            return RunQuery();
         }
 
         public void InitFixtures() 
         {
-            var conn = new NpgsqlConnection(_connString);
-            var samplesql = File.ReadAllText(System.AppContext.BaseDirectory + "/sample.sql");
+            var samplesql = File.ReadAllText("sample.sql");
 
-            using(conn)
-            {
-                conn.Open();
-
-                var tableCount = (Int64)conn.ExecuteScalar("select count(*) from information_schema.tables where table_schema = 'public';");
-                if(tableCount > 0) { throw new Exception("The test database in not empty"); }
-
-                var schemaCommand = new NpgsqlCommand(samplesql, conn);
-                schemaCommand.ExecuteNonQuery();
-            }
-        }
-
-        public void RunQuery() 
-        {
             try
             {
                 _conn.Open();
-                using(var trans = _conn.BeginTransaction())
-                {
-
-                }            
+                var createFixture = new NpgsqlCommand(samplesql, _conn);
+                createFixture.ExecuteNonQuery();
             }
             finally
             {
                 _conn.Close();
             }
+        }
+
+        public async Task RunQuery() 
+        {
+            var conn = new NpgsqlConnection(_connString);
+
+            try
+            {
+                conn.Open();
+                using(var trans = conn.BeginTransaction())
+                {
+                    // It returns the correct exception if you remove asynchronicity up to the root (main method).
+                    // Another key to this bug is that there must still be something left in the reader to read, 
+                    // if the exception is in the last (or only) statement, it also works correctly.
+                    var reader = await conn.QueryMultipleAsync(
+                        @"SELECT * FROM sample;
+                          SELECT * FROM sample;
+                        "
+                        , null, trans);
+
+                    var samples = reader.Read<Sample>();
+                    var nextSamples = reader.Read<Sample>();
+                }            
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return;
         }
 
         public class JsonTypeHandler: SqlMapper.TypeHandler<Json> {
@@ -86,6 +106,12 @@ namespace ConsoleApplication
         public class Json: Dictionary<Object, Object>
         {
 
+        }
+
+        public class Sample 
+        {
+            public int Id { get; set; }
+            public Json Metadata { get; set; }
         }
     }
 }
--- sample.sql
+++ sample.sql
@@ -1,12 +1,13 @@
-DROP TABLE IF EXISTS sample
-
+DROP TABLE IF EXISTS sample;
 CREATE TABLE sample (
     id integer NOT NULL,
-    metadata jsonb,
+    metadata jsonb
 );
+ALTER TABLE sample OWNER TO postgres;
 
-ALTER TABLE comment OWNER TO postgres;
+ALTER TABLE ONLY sample
+    ADD CONSTRAINT sample_pkey PRIMARY KEY (id);
 
-INSERT INTO sample (id, metadata) VALUES (1, '{}');
-INSERT INTO sample (id, metadata) VALUES (1, '{"ok": "test"}');
-INSERT INTO sample (id, metadata) VALUES (1, 'Failure');
+INSERT INTO sample (id, metadata) VALUES (1, '{}'::jsonb);
+INSERT INTO sample (id, metadata) VALUES (2, '{"ok": "test"}'::jsonb);
+INSERT INTO sample (id, metadata) VALUES (3, '"Failure"'::jsonb);
